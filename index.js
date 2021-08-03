0981ab6b3c16731710799b43b1f76795a13f4e2b
Alankar Saxena <saxenaalankar42@gmail.com>
2021-08-04 00:36:29 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http=require("http");
 const fs=require("fs");
-const requests=require('requests');
+const axios=require('axios');
 const console = require("console");
 
 const homeFile=fs.readFileSync("home.html", "utf-8");
@@ -14,26 +14,19 @@ const replaceVal=(tempval,orgval) =>{
     return temp;
 };
 
-const server=http.createServer((req,res)=>{
-    if(req.url =="/"){
-        requests("http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=9c7409bca6f5eff3a6280f768ac67c62")
-.on('data', (chunk) =>{
-    const objdata=JSON.parse(chunk);
-    const Data=[objdata];
-    
-
-    const realData=Data.map((val)=>replaceVal(homeFile,val)).join("");
-    res.write(realData);
-    
-})
-.on('end', (err) => {
-  if (err) return console.log('connection closed due to errors', err);
-  res.end();
-  
-  
-});
-}
-    
+const server=http.createServer( async (req,res)=>{
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(homeFile);
+    if(req.url === "/"){
+        const resp = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=9c7409bca6f5eff3a6280f768ac67c62")
+        const Data=[resp.data];
+        const realData=Data.map((val)=>replaceVal(homeFile,val)).join("");
+        console.log(realData);
+        res.write(realData);
+    }
+    res.end();
 });
 
-server.listen(3000);
+server.listen(3000, '127.0.0.1', () => {
+    console.log("Server Started!!!")
+});
